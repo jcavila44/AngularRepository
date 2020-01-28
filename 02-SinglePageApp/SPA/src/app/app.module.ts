b2273a1b9365f5c5195f8601600f0e768e827749
jcavila44 <ecommerce@grupoasistencia.com>
2020-01-28 17:30:33 -0500

    Routing, Services

--- 02-SinglePageApp/SPA/src/app/app.module.ts
+++ 02-SinglePageApp/SPA/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Rutas
 import { APP_ROUTING } from './app.routes';
+
+// Servicios
+import { HeroesService } from './servicios/heroes.service';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +20,16 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 02-SinglePageApp/SPA/src/app/components/heroes/heroes.component.ts
+++ 02-SinglePageApp/SPA/src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
 })
+
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heores: Heroe[];
+
+  constructor(private _heroesServices: HeroesService) {
+  }
 
   ngOnInit() {
+    this.heores = this._heroesServices.getHeores();
   }
+
 
 }
